0793600daa85a71ffd23ecd1e9ad72da9430cc96
marinel <marinel.bosna00@e-uvt.ro>
2022-01-03 01:48:06 +0200

    Laboratory 5 - add Align left, right and center classes + AlignStrategy interface

--- ProjectSP/src/Paragraph.java
+++ ProjectSP/src/Paragraph.java
@@ -1,13 +1,22 @@
 public class Paragraph extends Element{
 
     private String text;
+    private AlignStrategy textAlignment;
 
     public Paragraph(String text){
+
         this.text=text;
+        this.textAlignment = textAlignment;
+    }
+
+    public void setAlignStrategy(AlignStrategy strategy){
+        this.textAlignment=strategy;
     }
 
     public void print() {
-        System.out.println("Paragraph: "+this.text);
+        if(this.text != null && this.textAlignment != null) {
+            this.textAlignment.render(this.text);
+        }
     }
 
     @Override
